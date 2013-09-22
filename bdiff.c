13b1407b37dccb5f56ac658a6a40107f3110403c
rofl0r <retnyg@gmx.net>
2013-09-22 22:16:16 +0200

    add bdiff, a tool to list differences in binaries

---
+++ bdiff.c
@@ -0,0 +1,71 @@
+#include <inttypes.h>
+#include <stddef.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#ifdef USE_LIBULZ
+#include <ulz/stdio-repl.h>
+#endif
+
+static int lgetc(int fd) {
+	unsigned char buf;
+	int res;
+	if((res = read(fd, &buf, 1)) <= 0) return res;
+	return buf;
+}
+
+static int fetch(int fd, off_t *cnt) {
+	int c = lgetc(fd);
+	if(c >= 0) *cnt = *cnt + 1;
+	return c;
+}
+
+static int diff(int f1, int f2) {
+	off_t min;
+	size_t diffs = 0;
+	struct stat st1, st2;
+	if(fstat(f1, &st1)) return 1;
+	if(fstat(f2, &st2)) return 1;
+	min = st1.st_size;
+	size_t l;
+	off_t p1, p2;
+	if (st2.st_size != min) {
+		printf("sizes differ! %zu, %zu\n", st1.st_size, st2.st_size);
+		if (st2.st_size < min) min = st2.st_size;
+	}
+	p1 = 0;
+	p2 = 0;
+	while(p1 < min) {
+		l = 0;
+		while(p1 < min && fetch(f1, &p1) != fetch(f2, &p2)) l++;
+		if(l) {
+			printf("difference at offset %zu of size %zu\n", p1-l-1,  l);
+			diffs++;
+		}
+	}
+	printf("%zu differences in %zu bytes detected.\n", diffs, min);
+	return diffs != 0;
+}
+
+
+static void syntax(void) {
+	printf("syntax: filename1 filename2\nlist differences in binaries\n");
+	exit(1);
+}
+
+static int canread(char* fn) {
+	return access(fn, R_OK) != -1;
+}
+
+int main(int argc, char** argv) {
+	if(argc < 3) syntax();
+	if(!canread(argv[1]) || !canread(argv[2])) {
+		perror("can not read");
+		return 1;
+	}
+	return diff(open(argv[1], O_RDONLY), open(argv[2], O_RDONLY));
+}
