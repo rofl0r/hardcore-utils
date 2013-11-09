adf560a45550f818cf5655dcbb2675b79e02d0e8
rofl0r <retnyg@gmx.net>
2013-11-09 16:23:22 +0100

    su: major overhaul, using a better command line parser - arguments are non-exclusive anymore - username arg can be placed wherever you like - supports -s shell now (root only)

--- su.c
+++ su.c
@@ -52,10 +52,11 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 static const char usage_text[] =
 "Usage: su [OPTION] name\n"
-"available options (exclusive):\n"
+"available options:\n"
 "- : start login shell\n"
 "-c command : run command and return\n"
-"\nif name is omitted, root is assumed.\n";
+"-s shell   : use shell provided as argument (root only)\n"
+"if name is omitted, root is assumed.\n";
 
 static _Noreturn void usage(void) {
 	dprintf(1, usage_text);
@@ -65,14 +66,6 @@ static _Noreturn void usage(void) {
 static _Noreturn void perror_exit(const char* msg) {
 	perror(msg);
 	exit(1);
-}
-
-static int is_c_option(const char* arg) {
-	return arg[0] == '-' && arg[1] == 'c' && arg[2] == 0;
-}
-
-static int is_login_option(const char* arg) {
-	return arg[0] == '-' && arg[1] == 0;
 }
 
 static int directory_exists(const char* dir) {
@@ -88,7 +81,63 @@ static time_t get_mtime(const char *fn) {
 
 static void touch(const char* fn) {
 	int fd = open(fn, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if(fd != -1) close(fd); 
+	if(fd != -1) close(fd);
+}
+
+/* returns index of username argument, 0 if no username passed, -1 on syntax error
+ * cmd_index will be set to the index of a -c argument, or 0 if its missing
+ * shell_index will be set to the index of a -s argument, or 0 if its missing
+ * login_shell will be set to 1 if the login shell option - was passed, 0 otherwise */
+static int parse_args(int argc, char** argv,
+	             int *login_shell, int* cmd_index, int *shell_index) {
+
+	*login_shell = 0;
+	*cmd_index   = 0;
+	*shell_index = 0;
+
+	int i, wantarg = 0, res = 0;
+	char *p;
+
+	for(i = 1; i < argc; i++) {
+		if(wantarg) {
+			wantarg = 0;
+			continue;
+		}
+		p = argv[i];
+		if(p[0] != '-') {
+			if(res) return -1;
+			res = i;
+		} else if(!p[1]) {
+			*login_shell = 1;
+		} else if((p[1] == 'c' || p[1] == 's') && !p[2]) {
+			if(i == argc - 1) return -1;
+			wantarg = 1;
+			if(p[1] == 'c') *cmd_index = i + 1;
+			else *shell_index = i + 1;
+		} else {
+			return -1;
+		}
+	}
+	if(wantarg) return -1;
+	return res;
+}
+
+/* return 1 if the password check succeeded, 0 otherwise */
+int check_pass(const char* name, const char *pass, struct passwd *pwd) {
+        const char *encpw;
+#ifdef HAVE_SHADOW
+	struct spwd *shpwd = getspnam(name);
+	if(!shpwd) return 0;
+	encpw = shpwd->sp_pwdp;
+#else
+	(void) name;
+	encpw = pwd->pw_passwd;
+#endif
+	if(!encpw || !strcmp(encpw, "x")) return 0;
+	const char* actpw = crypt(pass, encpw);
+	if(!actpw) perror_exit("crypt");
+	if(strcmp(actpw, encpw)) return 0;
+	return 1;
 }
 
 extern char** environ;
@@ -100,52 +149,34 @@ extern char** environ;
 #define LOGIN_DELAY_SECS_STR STRIFY(LOGIN_DELAY_SECS)
 
 int main(int argc, char** argv) {
-	int nameindex = -1;
-	int login_shell = 0;
-	int cmd_index = 0;
-	if(argc == 1 || argc > 4) usage();
-	if(argc == 4) {
-		if(!is_c_option(argv[1])) usage();
-		else {
-			cmd_index = 1;
-			nameindex = 3;
-		}
-	} else if (argc == 3) {
-		if(is_login_option(argv[1])) {
-			nameindex = 2;
-			login_shell = 1;
-		} else if(is_c_option(argv[1])) {
-			nameindex = 0;
-			cmd_index = 1;
-		} else usage();
-	} else if (argc == 2) {
-		if(is_login_option(argv[1])) {
-			nameindex = 0;
-			login_shell = 1;
-		} else
-			nameindex = 1;
-	}
-	if(nameindex == -1) usage();
-	const char* name = nameindex ? argv[nameindex] : "root";
-	if(name[0] == '-') usage();
+	int login_shell, cmd_index, shell_index, name_index;
+	name_index = parse_args(argc, argv, &login_shell, &cmd_index, &shell_index);
+	if(name_index == -1) usage();
+
 	int uid = getuid();
+	int is_root = (uid == 0);
+	if(shell_index && !is_root) usage();
+
+	const char* name = name_index ? argv[name_index] : "root";
 
 	char uidfn[256];
 	snprintf(uidfn, sizeof uidfn, "%s/%d", SU_DIR, uid);
 
 	if(!directory_exists(SU_DIR)) {
 		if(geteuid() == 0 && mkdir(SU_DIR, 0700) == -1)
-			dprintf(2, "creation of directory " SU_DIR " for bruteforce prevention failed, consider creating it manually\n");
+			dprintf(2, "creation of directory " SU_DIR
+			           " for bruteforce prevention failed, consider creating it manually\n");
 	}
-	if(uid) {
+
+	char* pass = 0;
+
+	if(!is_root) {
 		time_t mtime = get_mtime(uidfn);
 		if(mtime != -1 && mtime + LOGIN_DELAY_SECS > time(0)) {
-			dprintf(2, "you need to wait for " LOGIN_DELAY_SECS_STR " seconds before retrying.\n");
-			exit(1);
+			dprintf(2, "you need to wait for " LOGIN_DELAY_SECS_STR
+			           " seconds before retrying.\n");
+			return 1;
 		}
-	}
-	char* pass = 0;
-	if(uid) {
 		pass = getpass("enter password:");
 		dprintf(1, "\n");
 		if(!pass) perror_exit("getpass");
@@ -153,42 +184,31 @@ int main(int argc, char** argv) {
 
 	struct passwd *pwd = getpwnam(name);
 	if(!pwd) goto failed;
-	if(!uid) goto success;
-
-	const char *encpw;
-#ifdef HAVE_SHADOW
-	struct spwd *shpwd = getspnam(name);
-	if(!shpwd) goto failed;
-	encpw = shpwd->sp_pwdp;
-#else
-	encpw = pwd->pw_passwd;
-#endif
-	if(!encpw || !strcmp(encpw, "x")) goto failed;
-	const char* actpw = crypt(pass, encpw);
-	if(!actpw) perror_exit("crypt");
-	if(strcmp(actpw, encpw)) {
+	if(!is_root && !check_pass(name, pass, pwd)) {
 		failed:
-		if(uid) touch(uidfn);
-		dprintf(2, "login failed\n");
+                touch(uidfn);
+                dprintf(2, "login failed\n");
 		return 1;
 	}
-	success:;
+
 	if(initgroups(name, pwd->pw_gid)) perror_exit("initgroups");
 	if(setgid(pwd->pw_gid)) perror_exit("setgid");
 	if(setuid(pwd->pw_uid)) perror_exit("setuid");
+
 	char* const* new_argv;
 	char shellbuf[256];
-	char* shell = pwd->pw_shell;
+	char* shell = shell_index ? argv[shell_index] : pwd->pw_shell;
+	char* shell_argv0 = shell;
 	if(login_shell) {
-		snprintf(shellbuf, sizeof shellbuf, "-%s", pwd->pw_shell);
-		new_argv = (char* const[]) {shell, shellbuf, 0};
+		snprintf(shellbuf, sizeof shellbuf, "-%s", shell);
+		shell_argv0 = shellbuf;
 		setenv("LOGNAME", name, 1);
 		if(getenv("USER")) setenv("USER", name, 1);
-	} else if(cmd_index) {
-		shell = "/bin/sh";
-		new_argv = (char* const[]) {shell, argv[cmd_index], argv[cmd_index+1], 0};
+	}
+	if(cmd_index) {
+		new_argv = (char* const[]) {shell_argv0, argv[cmd_index-1], argv[cmd_index], 0};
 	} else {
-		new_argv = (char* const[]) {shell, 0};
+		new_argv = (char* const[]) {shell_argv0, 0};
 	}
 
 	setenv("HOME", pwd->pw_dir, 1);
